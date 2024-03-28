581fbbc790aa895bcae5a110a05ac7dca84ffc76
CT7WD <63139550+CT7WD@users.noreply.github.com>
2024-03-28 15:34:18 -0400

    Upload Assessment Files
    Upload Assessment Files

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+
+const users = require("./data/users-data");
+const states = require("./data/states-data");
+
+// TODO: Return a single user by id from /users/:userId in form of { data: Object }
+app.use(
+  "/users/:userId",
+  (req, res, next) => {
+    const { userId } = req.params;
+    const foundUser = users.find((user) => user.id === Number(userId));
+    if(foundUser) {
+      res.json({ data: foundUser });
+    } else {
+      next(`User ID not found: ${userId}`)
+    }
+  }
+);
+
+
+// TODO: Return an array of users from /users in form of { data: Array }
+app.use(
+  "/users",
+  (req, res) => {
+    res.json({ data: users });
+  }
+);
+
+
+
+// TODO: Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
+app.use(
+  "/states/:stateCode",
+  (req, res, next) => {
+    const { stateCode } = req.params;
+    const foundState = states[stateCode];
+    if (foundState) {
+      res.json({ data: {stateCode: stateCode, name:foundState}});
+    } else {
+      next(`State code not found: ${stateCode}`);
+    }
+  }
+);
+
+
+
+// TODO: Return all states from /states in the form of { data: Array }
+app.use(
+  "/states",
+  (req, res) => {
+    res.json({ data: states });
+  }
+);
+
+
+
+
+// TODO: Add not-found handler.
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
+
+// TODO: Add error handler.
+app.use((error, request, response) => {
+  console.error(error);
+  response.send(error);
+});
+
+module.exports = app;
